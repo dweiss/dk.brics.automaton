d565ada0a4d854857864f33e1be3fadad75986bf
Dawid Weiss <dawid.weiss@carrot-search.com>
2011-03-20 11:17:35 +0100

    Adding a regexp test that shows a similar behavior.

--- src-test/dk/brics/automaton/StringUnionOperatorsTest.java
+++ src-test/dk/brics/automaton/StringUnionOperatorsTest.java
@@ -1,8 +1,9 @@
 package dk.brics.automaton;
 
-import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringUnionOperatorsTest
@@ -14,15 +15,10 @@ public class StringUnionOperatorsTest
         {
             "abc", "abcdef"
         };
-        Arrays.sort(input, dk.brics.automaton.StringUnionOperations.LEXICOGRAPHIC_ORDER);
 
-        final State rootState = dk.brics.automaton.StringUnionOperations.build(input);
-        final Automaton wordsPattern = new Automaton();
-        wordsPattern.setInitialState(rootState);
-        wordsPattern.restoreInvariant();
+        final Automaton wordsPattern = Automaton.makeStringUnion(input);
 
         RunAutomaton runAutomaton = new RunAutomaton(wordsPattern);
-        System.out.println(wordsPattern.toDot());
 
         assertTrue(runAutomaton.run( "abc"));
         assertFalse(runAutomaton.run("abcde"));
@@ -30,6 +26,21 @@ public class StringUnionOperatorsTest
         assertFalse(runAutomaton.run( "abcdefg"));
 
         AutomatonMatcher m = runAutomaton.newMatcher("abcde");
+        assertTrue(m.find());
+        assertEquals("abc", m.group());
+    }
+
+    @Test
+    public void testBuildFromSortedArray2()
+    {
+        final Automaton runAutomaton = new RegExp("(abc)|(abcdef)").toAutomaton();
+
+        assertTrue(runAutomaton.run( "abc"));
+        assertFalse(runAutomaton.run("abcde"));
+        assertTrue(runAutomaton.run( "abcdef"));
+        assertFalse(runAutomaton.run( "abcdefg"));
+
+        AutomatonMatcher m = new RunAutomaton(runAutomaton).newMatcher("abcde");
         assertTrue(m.find());
         assertEquals("abc", m.group());
     }
